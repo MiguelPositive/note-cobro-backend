e31f11641742a28b8393bd62f21b77ea79a06aab
miguel <miguelpositive@gmail.com>
2023-09-21 16:55:50 -0500

    cors add

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import connectionDb from "./config/connectionDb.js";
 
 import usersRouter from "./routes/usersRouter.js";
 
+import cors from "cors";
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -14,6 +16,10 @@ app.use(express.json());
 
 configDotenv();
 connectionDb();
+
+//Los cors por defecto evita que servidores externos puedan hacer solicitudes a mi back
+//Aqui eestoy desactivando esa configuración.
+app.use(cors());
 
 app.use("/", usersRouter);
 
