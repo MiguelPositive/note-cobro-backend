6352011fd3c57812803d9794129957da486c4350
miguel <miguelpositive@gmail.com>
2023-09-21 16:02:54 -0500

    users controller created

---
+++ controllers/usersController.js
@@ -0,0 +1,20 @@
+import usersModel from "../models/usersModel";
+
+const createUser = async (req, res) => {
+  const { user, password } = req.body;
+
+  const newUser = await usersModel({ user, password });
+
+  res.sendStatus(200);
+
+  newUser.save();
+
+  try {
+  } catch (error) {
+    console.log(
+      `ocurrio un erro al intentar crear el usuario en el backend: ${error}`
+    );
+  }
+};
+
+export default createUser;
